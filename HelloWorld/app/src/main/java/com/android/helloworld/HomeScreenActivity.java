26ea3f35f9e5e2ce217c2c8753b95203fcc771f6
AnandaAp <esperasta1>
2020-10-09 08:37:38 +0700

    menambahkan new activity

--- HelloWorld/app/src/main/java/com/android/helloworld/HomeScreenActivity.java
+++ HelloWorld/app/src/main/java/com/android/helloworld/HomeScreenActivity.java
@@ -7,18 +7,13 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class HomeScreenActivity extends AppCompatActivity {
-    private TextView email;
-    private TextView password;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
         Intent intent = getIntent();
-        String emailShow = intent.getStringExtra(MainActivity.EXTRA_MESSAGE1);
-        String passShow = intent.getStringExtra(MainActivity.EXTRA_MESSAGE2);
-        email = findViewById(R.id.emailUser);
-        email.setText(emailShow);
-        password = findViewById(R.id.passUser);
-        password.setText(passShow);
+
     }
 }
