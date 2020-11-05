72aca467642aef269d7e619c81ebd83c92d4f1dd
AnandaAp <42529423+anandaap@users.noreply.github.com>
2020-11-05 15:57:28 +0700

    Pertemuan 6 latihan 2 membuat recycleview pada fragment ProfileFragment yang menampilkan informasi tentang logo film, judul, dan sinopsisnya.

--- HelloWorld/app/src/main/java/com/android/helloworld/ProfileFragment.java
+++ HelloWorld/app/src/main/java/com/android/helloworld/ProfileFragment.java
@@ -3,13 +3,22 @@ package com.android.helloworld;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class ProfileFragment extends Fragment {
-
+    String[] film;
+    String[] sinopsis;
+    RecyclerView recyclerView;
+    int[] image ={R.drawable.wonder_woman,R.drawable.trolls_world,R.drawable.thecroods
+            ,R.drawable.spongebob,R.drawable.mulan,R.drawable.bad_boy,R.drawable.a_quite_place
+            ,R.drawable.peter_rabbit,R.drawable.birds_of_prey,R.drawable.dolittel,R.drawable.onward
+            ,R.drawable.the_invicible_man,R.drawable.sonic,R.drawable.free_guy
+    };
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +27,13 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        film = getResources().getStringArray(R.array.ListFilm);
+        sinopsis = getResources().getStringArray(R.array.Sinopsis);
+        recyclerView = view.findViewById(R.id.recicleView);
+        RecycleViewAdapter recycleViewAdapter = new RecycleViewAdapter(requireContext(),film,sinopsis,image);
+        recyclerView.setAdapter(recycleViewAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+        return view;
     }
 }
